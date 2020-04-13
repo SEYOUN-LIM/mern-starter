e84097957b1268e5f58f30574e133398d63641af
Seyoun Lim <imseyoun@gmail.com>
2020-04-13 20:03:42 +1200

    login register page logic

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,9 +1,17 @@
-import { LOGIN_USER } from '../_actions/types';
+import { LOGIN_USER, REGISTER_USER, AUTH_USER } from '../_actions/types';
 
 export default function (previousState = {}, action) {
   switch (action.type) {
     case LOGIN_USER:
       return { ...previousState, loginSuccess: action.payload };
+      break;
+
+    case REGISTER_USER:
+      return { ...previousState, register: action.payload };
+      break;
+
+    case AUTH_USER:
+      return { ...previousState, userData: action.payload };
       break;
 
     default:
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,20 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get('/api/hello').then((response) => console.log(response));
   }, []);
+
+  const onLogoutHandler = () => {
+    axios.get(`/api/users/logout`).then((response) => {
+      if (response.data.success) {
+        props.history.push('/login');
+      } else {
+        alert('로그아웃 실패');
+      }
+    });
+  };
 
   return (
     <div
@@ -12,11 +22,14 @@ function LandingPage() {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        flexDirection: 'column',
         width: '100%',
         height: ' 100vh',
       }}
     >
       <h2>시작 페이지</h2>
+
+      <button onClick={onLogoutHandler}>로그아웃</button>
     </div>
   );
 }
